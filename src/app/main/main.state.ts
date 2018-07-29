7d2bc38893dd2bd5dc7f8b567d22b670b7ddbabd
Louis Byrne <lbyrne@aktisintel.com>
2018-07-29 14:38:12 +0100

    created initial state, interface and factory for main resource.

--- src/app/main/main.state.ts
+++ src/app/main/main.state.ts
@@ -1,10 +1,40 @@
 import { makeTypedFactory, TypedRecord } from 'typed-immutable-record';
+import * as Immutable from 'immutable';
+
+
+interface IEventsState {
+    minute: number;
+    type: string;
+    text: string;
+    player: string;
+}
+
+const Events: IEventsState = {
+    minute: null,
+    type: null,
+    text: null,
+    player: null,
+};
+
+export const EventsStateFactory = makeTypedFactory<IEventsState, IEventsStateRecord>(Events);
+
+export interface IEventsStateRecord extends TypedRecord<IEventsStateRecord>, IEventsState { };
 
 
 interface IMainState {
+    home: string;
+    away: string;
+    attendance: string;
+    referee: string;
+    events: Immutable.List<IEventsStateRecord>;
 }
 
 const Main: IMainState = {
+    home: null,
+    away: null,
+    attendance: null,
+    referee: null,
+    events: Immutable.List(),
 };
 
 export const MainStateFactory = makeTypedFactory<IMainState, IMainStateRecord>(Main);
