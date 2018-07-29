f409846393eb551eadb2c2cdbe95ae46b651ce94
Louis Byrne <lbyrne@aktisintel.com>
2018-07-29 14:33:52 +0100

    added method do download json.

--- src/app/main/main.actions.ts
+++ src/app/main/main.actions.ts
@@ -1,5 +1,10 @@
 import { NgRedux } from '@angular-redux/store';
 import { Injectable } from '@angular/core';
+import { Subject } from 'rxjs';
+import { HttpClient } from '@angular/common/http';
+import { Response } from '@angular/http';
+
+
 
 import { IAppStateRecord } from '../store/index.state';
 
@@ -8,5 +13,18 @@ export class MainActions {
   static RESET = 'main/RESET';
   constructor(
     private store: NgRedux<IAppStateRecord>,
+    private http: HttpClient
   ) { }
+
+  public initialiseDataStream(): void {
+    this.http.get('./assets/data.json')
+    .subscribe((response: Response): void => 
+      this.addDataToStore(response)
+    );
+
+  }
+
+  private addDataToStore(data: any): void {
+    
+  }
 }
--- src/app/main/main.module.ts
+++ src/app/main/main.module.ts
@@ -1,13 +1,20 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { MainComponent } from './main.component';
+import { MainActions } from './main.actions';
+import { HttpClient, HttpClientModule } from '@angular/common/http'; 
 
 @NgModule({
   imports: [
-    CommonModule
+    CommonModule,
+    HttpClientModule
   ],
   declarations: [
     MainComponent
+  ],
+  providers: [
+    MainActions,
+    HttpClient,
   ]
 })
 export class MainModule { }
